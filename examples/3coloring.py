c643dd49d16e589ef098c9de71c5f4b5371582a2
Jakob Rath <mail@jakobrath.eu>
2016-06-03 19:03:32 +0200

    Add example

---
+++ examples/3coloring.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+
+import dlvhex
+import os.path as p
+
+
+class Node:
+    def __init__(self, label):
+        self.label = label
+
+
+def main():
+    dlvhex.debug = True
+
+    asp_file = p.join(p.dirname(p.realpath(__file__)), '3coloring.dl')
+
+    prog = dlvhex.Program(filename=asp_file)
+
+    na = Node('a')
+    nb = Node('b')
+    nc = Node('c')
+    nodes = [na, nb, nc]
+    edges = {
+        na: [nb, nc],
+        nb: [nc]
+    }
+
+    rs = prog.solve(nodes, edges, cache=True)
+    print(rs)
+    for i, x in enumerate(rs):
+        print(i, repr(x))
+
+    for i, x in enumerate(rs):
+        print(i, repr(x))
+
+    for ans in prog(nodes, edges):
+        print(ans)
+
+
+if __name__ == '__main__':
+    main()
