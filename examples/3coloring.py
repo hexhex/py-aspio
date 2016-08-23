f85448b1ed60da546c8dc07547b2bd477236e27d
Jakob Rath <mail@jakobrath.eu>
2016-08-23 13:19:30 +0200

    Change example

--- examples/3coloring.py
+++ examples/3coloring.py
@@ -1,14 +1,12 @@
 #!/usr/bin/env python3
-
-import dlvhex
-import logging
-import os.path as p
 from collections import namedtuple
+from pathlib import Path
+import logging
+import dlvhex
 
 # Enable debug messages from dlvhex
 dlvhex.log.addHandler(logging.StreamHandler())
 dlvhex.log.setLevel(logging.DEBUG)
-
 
 Node = namedtuple('Node', ['label'])
 ColoredNode = namedtuple('ColoredNode', ['label', 'color'])
@@ -18,22 +16,35 @@ dlvhex.register_dict(globals())
 
 
 def main():
-    asp_file = p.join(p.dirname(p.realpath(__file__)), '3coloring.dl')
-
+    # Load ASP program and input/output specifications from file
+    asp_file = Path(__file__).with_name('3coloring.dl')
     prog = dlvhex.Program(filename=asp_file)
 
-    na = Node('a')
-    nb = Node('b')
-    nc = Node('c')
-    nodes = [na, nb, nc]
+    # Some sample data
+    a = Node('a')
+    b = Node('b')
+    c = Node('c')
+    nodes = [a, b, c]
     arcs = [
-        Arc(na, nb),
-        Arc(na, nc),
-        Arc(nb, nc)
+        Arc(a, b),
+        Arc(a, c),
+        Arc(b, c)
     ]
 
-    for i, colored_nodes in enumerate(prog.solve(nodes, arcs).all_colored_nodes):
-        print('Answer set {0}:\t{1!r}'.format(i + 1, sorted(colored_nodes)))
+    # Iterate over all answer sets
+    for result in prog.solve(nodes, arcs):
+        print(result.colored_nodes)
+
+    # A shortcut if only one output variable is needed
+    for colored_nodes in prog.solve(nodes, arcs).all_colored_nodes:
+        print(colored_nodes)
+
+    # Compute a single answer set, or return None if no answer set exists
+    result = prog.solve_one(nodes, arcs)
+    if result is not None:
+        print(result.colored_nodes)
+    else:
+        print('no answer set')
 
 if __name__ == '__main__':
     main()
